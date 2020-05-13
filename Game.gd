7bc4bf23e731e2432ed8f27b7f943242e5d3b2c3
gioele <gioele.santi@gmail.com>
2020-05-13 12:47:21 +0200

    Game scene update
    Fixed error in spawn. Now add a piece at a time.

--- Game.gd
+++ Game.gd
@@ -1,9 +1,17 @@
 extends Node
 
 onready var spawn_timer := $Spawn_timer
+export (PackedScene) var Bird_scene
 
 func _ready() -> void:
 	pass # Replace with function body.
+
+func spawn_bird() -> void:
+	var start_point = Vector2(rand_range(0.0, 1024.0), 0.0)
+	var new_bird: Bird = Bird_scene.instance()
+	new_bird.initialize(start_point)
+	#connect die, land fly_away signals
+	$Birds.add_child(new_bird)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("button_1"):
