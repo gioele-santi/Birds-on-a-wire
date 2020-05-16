8e2baa31d4fcd61d419e80e11c1b001afd30c788
gioele <gioele.santi@gmail.com>
2020-05-16 10:39:46 +0200

    Changed progres bar's verse

--- AntennaPowerBar.gd
+++ AntennaPowerBar.gd
@@ -1,8 +1,14 @@
 extends Control
 class_name AntennaPowerBar
 
+export (bool) var inverted := false setget set_inverted
+
 func _ready() -> void:
-	$TextureProgress.modulate = Color("4e58e4")
+	$TextureProgress.modulate = Color("cfa3ff")
 
 func _on_Antenna_update_power(value) -> void:
 	$TextureProgress.value = value
+
+func set_inverted(value) -> void:
+	inverted = value
+	$TextureProgress.fill_mode = 1 if inverted else 0
